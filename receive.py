6fe28f1ae5de4775daa64a8e44b253f85725e486
Leo <leonardom1808@gmail.com>
2019-08-30 10:16:26 -0300

    Uso de funcoes

--- receive.py
+++ receive.py
@@ -14,11 +14,49 @@ print("comecou")
 from enlace import *
 import time
 
+def removeEOP(rxBuffer2):
+    for i in range (len(rxBuffer2)):
+    
+            if i >3 :
+                
+                if (rxBuffer2[i] == 122 and rxBuffer2[i-1] == 122 and rxBuffer2[i-2]==122  and rxBuffer2[i-3] == 122):
+                    if rxBuffer2[i-3] ==rxBuffer2[-4]:
+                        del rxBuffer2[i]
+                        del rxBuffer2[i-1]
+                        del rxBuffer2[i-2]
+                        del rxBuffer2[i-3]
+                    else:
+                        print(rxBuffer2[i-3])
+                        print(rxBuffer2[-4])
+                        print("EOP está num lugar inesperado.")
+                        break
+    return rxBuffer2
+
+
+def removeStuffing(rxBuffer2):
+    z = 0
+    for i in range(len(rxBuffer2)):
+        if i >4:
+            if (rxBuffer2[i] == 122 and rxBuffer2[i-1] == 122 and rxBuffer2[i-2]==0  and rxBuffer2[i-3] == 122 and rxBuffer2[i-4] == 122):
+                z+=1
+                    
+    for i in range (len(rxBuffer2)-z):
+        if i >4:
+            if (rxBuffer2[i] == 122 and rxBuffer2[i-1] == 122 and rxBuffer2[i-2]==0  and rxBuffer2[i-3] == 122 and rxBuffer2[i-4] == 122):
+                del rxBuffer2[i-2]
+    return rxBuffer2
+
+def toByte(rxBuffer2, lista):
+    for x in rxBuffer2:
+        lista.append(x)
+        rxBuffer2 = bytes(lista)
+    return rxBuffer2
+
 # Serial Com Port
 #   para saber a sua porta, execute no terminal :
 #   python3 -m serial.tools.list_ports
 
-serialName = "/dev/ttyACM1"           # Ubuntu (variacao de)
+serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
 #serialName = "/dev/cu.usbmodem142101"
 #serialName = "COM4"                  # Windows(variacao de)
@@ -41,31 +79,6 @@ def main():
     # Carrega dados
     print ("gerando dados para transmissao :")
   
-      #no exemplo estamos gerando uma lista de bytes ou dois bytes concatenados
-    
-    #exemplo 1
-    #ListTxBuffer =list()
-    #for x in range(1,10):
-    #    ListTxBuffer.append(x)
-    #txBuffer = bytes(ListTxBuffer)
-    
-    #txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
-    
-    
-    #txBuffer = open("cloud9.jpg", "rb").read()
-    
-    # Transmite dado
-    #print("tentado transmitir .... {} bytes".format(txLen))
-    
-    
-    #com.sendData(txBuffer)
-
-    # espera o fim da transmissão
-    #while(com.tx.getIsBussy()):
-      #    pass
-    
-    
-    # Atualiza dados da transmissão
     
 
     # Faz a recepção dos dados
@@ -101,42 +114,15 @@ def main():
             print("Tamanho do próximo pacote: ", tamanho)
             rxBuffer2, nRx = com.getData(tamanho)
             
-     
 
-        rxBuffer2 = bytearray(rxBuffer2)
-
-
-        for i in range (len(rxBuffer2)):
-            
-            if i >3 :
-                
-                if (rxBuffer2[i] == 122 and rxBuffer2[i-1] == 122 and rxBuffer2[i-2]==122  and rxBuffer2[i-3] == 122):
-                    if rxBuffer2[i-3] !=rxBuffer2[-4]:
-                        print(rxBuffer2[i-3])
-                        print(rxBuffer2[-4])
-                        print("EOP está num lugar inesperado.")
-                        break
-                    else:
-                        print("EOP foi encontrado no lugar correto",i-3)
-                    del rxBuffer2[i]
-                    del rxBuffer2[i-1]
-                    del rxBuffer2[i-2]
-                    del rxBuffer2[i-3]
-        
-        z = 0
-        for i in range(len(rxBuffer2)):
-            if i >4:
-                if (rxBuffer2[i] == 122 and rxBuffer2[i-1] == 122 and rxBuffer2[i-2]==0  and rxBuffer2[i-3] == 122 and rxBuffer2[i-4] == 122):
-                    z+=1
-                    
-        for i in range (len(rxBuffer2)-z):
-            if i >4:
-                if (rxBuffer2[i] == 122 and rxBuffer2[i-1] == 122 and rxBuffer2[i-2]==0  and rxBuffer2[i-3] == 122 and rxBuffer2[i-4] == 122):
-                     del rxBuffer2[i-2]
-        
-        for x in rxBuffer2:
-            lista.append(x)
-        rxBuffer2 = bytes(lista)
+        #Converte o rxBuffer para bytearray para podermos alterá-lo
+        rxBuffer_bytearray = bytearray(rxBuffer2)
+        #Remove o End Of Package
+        rxBuffer_eop = removeEOP(rxBuffer_bytearray)
+        #Remove Stuffing
+        rxBuffer_stuff = removeStuffing(rxBuffer_eop)
+        #Converte de bytearray para uma lista em byte (Para poder salvar a imagem)
+        rxBuffer = toByte(rxBuffer_stuff, lista)
         
   
 
@@ -149,20 +135,10 @@ def main():
     print ("Lido              {} bytes ".format(nRx))
 
     
-    open("logo.png", "wb").write(rxBuffer2)
-    
+    open("logo.png", "wb").write(rxBuffer)
     print("Imagem salva")
     #print(rxBuffer2[:EOPstart-2].decode("utf-8"))
     print("------------------------------------------")
-
-
-    # log
-        
-    #print (rxBuffer)
-    #time.sleep(5)
-    #print("Enviando o len de volta")
-    #com.sendData(rxBuffer)
-    #print("Enviado")
     
          
     # Encerra comunicação
@@ -170,6 +146,8 @@ def main():
     print("Comunicação encerrada")
     print("-------------------------")
     com.disable()
+
+
 
 
     #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
